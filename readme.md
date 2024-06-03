26e5c53e66780627f450d8772962e10520f35198
Aster <AsterRdh@live.cn>
2024-06-03 13:11:01 +0800

    添加 ExcelUtils

--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # 这是一个为NCC二开的工具类、集合和语法糖的集合
-
+## 使用方法
+将jar包放入ncchome\external\lib目录下
 ## 工具类和语法糖
 1. CollectionUtils 数组、集合的一些常用方法
 2. SplitAndMergeFile 分割、合并文件的工具，比如将大文件存入文件服务器或者多线程分发文件时可以使用
@@ -10,7 +11,7 @@
 7. StringUtils 字符串相关工具
 8. MathUtils 针对UFDouble的数学工具
 9. SqlBuilder sql构造器，新版本开发中
-
+10. ExcelUtils Excel工具类，目前只有 列数和Excel列互转
 ## 集合
 1. MapList
 2. MapSet
---
+++ src/nccloud/utils/excel/ExcelUtils.java
@@ -0,0 +1,46 @@
+package nccloud.utils.excel;
+
+public class ExcelUtils {
+
+    /**
+     * 列数转Excel列
+     * @param col 列数 从1开始
+     * @return Excel列 如AA
+     */
+    public String caseToExcelColIndex(int col){
+        int temp = col ;
+        StringBuilder eCol = new StringBuilder();
+        while (temp > 26){
+            int i = temp % 26;
+            temp = temp / 26;
+            char a = (char) ( i + 64 );
+            eCol.insert(0, a);
+        }
+        char a = (char) ( temp + 64 );
+        eCol.insert(0, a);
+
+
+        return eCol.toString();
+    }
+
+    /**
+     * Excel列转列数
+     * @param excelCol Excel列 如AAA
+     * @return 列数 从1开始
+     */
+    public int caseToColIndex(String excelCol){
+        String temp = excelCol.toUpperCase();
+        int i = 0;
+
+        char c1 = temp.charAt(temp.length() - 1);
+        i += c1-64;
+
+        int mu = 0;
+        for (int length = excelCol.length()-1; length > 0; length--) {
+            char c = excelCol.charAt(length - 1);
+            i += 26 * (int)Math.pow(26,mu++) * (c-64);
+
+        }
+        return i;
+    }
+}
